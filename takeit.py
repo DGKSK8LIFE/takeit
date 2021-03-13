d6c9d5deba9c905b2779d94ad1853c76d6960b45
avcado <devavcado@gmail.com>
2021-03-13 16:13:59 -0600

    Added some arguments to use later

--- takeit.py
+++ takeit.py
@@ -7,3 +7,12 @@ config = dotenv_values("./.env")
 # get the client id and the token
 token = config['REDDIT_SECRET']
 Id    = config['REDDIT_ID']
+
+# setup user arguments
+# (reason why i have sys imported)
+arg1 = sys.argv[1] # 0 is the python script itself
+# 1 will be the subreddit
+arg2 = sys.argv[2] # 2 will be how many submissions needed.
+arg3 = sys.argv[3] # 3 will be the folder to download the images,
+# gifs or videos to.
+
