3c567fd71ee25ec68495578a6aa355fadd9ba5f9
avcado <devavcado@gmail.com>
2021-03-13 17:20:56 -0600

    Sortof added downloading

--- takeit.py
+++ takeit.py
@@ -1,6 +1,7 @@
 import praw
 from dotenv import dotenv_values
 from datetime import date
+from urllib import request # For downloading.
 import sys
 
 today = date.today()
@@ -15,7 +16,7 @@ Id    = config['REDDIT_ID']
 # (reason why i have sys imported)
 arg1 = str(sys.argv[1]) # 0 is the python script itself
 # 1 will be the subreddit
-arg2 = str(sys.argv[2]) # 2 will be how many submissions needed.
+arg2 = int(sys.argv[2]) # 2 will be how many submissions needed.
 arg3 = str(sys.argv[3]) # 3 will be the folder to download the images,
 # gifs or videos to.
 
@@ -25,4 +26,25 @@ takeit = praw.Reddit(
     client_secret=token,
     user_agent="takeit by /u/abcado"
 )
+
 print(f"Created REDDIT instance at {today.strftime('%m/%d, %Y')}")
+
+isNsfwEnabled = False # just default, because...
+# Start doing the submission downloading
+for i in range(arg2):
+    for submission in takeit.subreddit(arg1).hot(limit=arg2):
+        if submission.over_18:
+            if isNsfwEnabled == True:
+                request.urlretrieve(submission.url, arg3)      
+            else:
+                nsfwCheck = input("Do you want to enable nsfw for this session? (y/N) ")
+                if nsfwCheck == 'y':
+                    isNsfwEnabled = True
+                else:
+                    # Do NOT download the image.
+                    print(f"Not downloading image {i} of {arg2}")
+        else:
+            # Just normally download the image...
+            request.urlretrieve(submission.url, arg3)
+
+
