c13e77d2708ed135e38d73240075d6266aa1f390
avcado <devavcado@gmail.com>
2021-03-13 16:01:54 -0600

    Add base vars to takeit.py

--- takeit.py
+++ takeit.py
@@ -1,3 +1,9 @@
 import praw
 from dotenv import python-dotenv
 import sys
+
+# load config file
+config = dotenv_values("./.env")
+# get the client id and the token
+token = config['REDDIT_SECRET']
+Id    = config['REDDIT_ID']
